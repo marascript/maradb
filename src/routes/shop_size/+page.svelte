db07086fe23488fcae9dccfe4a106cbe16e00323
Billy <will.js@outlook.com>
2024-06-12 10:20:32 -0500

    Simpler formula, add constraints

--- src/routes/shop_size/+page.svelte
+++ src/routes/shop_size/+page.svelte
@@ -1,19 +1,27 @@
 <script>
-	let newCost = 0;
-	let currentSize = 0;
-	let desiredSize = 0;
+	let newCost = 50;
+	let currentSize = 1;
+	let desiredSize = 2;
 
 	function calculateSize() {
 		const neededLevels = desiredSize - currentSize;
 		const cost = currentSize * 50;
-		newCost = cost * neededLevels + 50 * (((neededLevels - 1) * (neededLevels - 1 + 1)) / 2);
+
+		newCost = cost * neededLevels + 50 * (((neededLevels - 1) * neededLevels) / 2);
 	}
 </script>
 
 <div>
 	<h1>Shop Size Calculator</h1>
-	<input bind:value={currentSize} on:change={calculateSize} type="number" />
-	<input bind:value={desiredSize} on:change={calculateSize} type="number" />
+	<input
+		bind:value={currentSize}
+		on:change={calculateSize}
+		type="number"
+		min="1"
+		max={desiredSize - 1}
+	/>
+	<input bind:value={desiredSize} on:change={calculateSize} type="number" min={currentSize + 1} />
 
+	<br />
 	{newCost}
 </div>
