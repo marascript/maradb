0e28452280c318c1aeed3d9c04597b2ec815f5a5
Billy <will.js@outlook.com>
2024-06-13 11:37:06 -0500

    Testing direct search links

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -3,7 +3,23 @@
 </script>
 
 <ul>
+	<!-- TODO: Forms work for a few times then stop -->
 	{#each data.items as item}
 		<li>{item.id} - {item.name} - {item.rarity}</li>
+		<form method="post" target="_blank" action="https://www.marapets.com/shopsearch.php?do=search">
+			<input name="type" hidden value="2" />
+			<input name="item" hidden value={item.name} />
+			<input type="submit" value="User Shops" />
+		</form>
+
+		<form
+			method="post"
+			target="_blank"
+			action="https://www.marapets.com/shopsearch.php?do=gallerysearch"
+		>
+			<input name="type" hidden value="2" />
+			<input name="item" hidden value={item.name} />
+			<input type="submit" value="Galleries" />
+		</form>
 	{/each}
 </ul>
