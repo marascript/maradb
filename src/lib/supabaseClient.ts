2f75554db6dbc5612e0e6f213975ce5c5289c94c
Billy <will.js@outlook.com>
2024-06-13 08:55:36 -0500

    Import Database types as types

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from './databaseTypes';
+import type { Database } from './databaseTypes';
 
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
